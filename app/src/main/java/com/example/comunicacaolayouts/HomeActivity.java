db3be52b21d134f4a59b2aae24a37dcfd3960fa5
martinsfelipemartins <martins.felipe1@hotmail.com>
2019-05-22 16:47:48 -0300

    continuacao do ex

--- app/src/main/java/com/example/comunicacaolayouts/HomeActivity.java
+++ app/src/main/java/com/example/comunicacaolayouts/HomeActivity.java
@@ -2,12 +2,29 @@ package com.example.comunicacaolayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        textView = findViewById(R.id.textView);
+
+        if (getIntent() != null && getIntent().getExtras() != null) {
+
+            Bundle bundle = getIntent().getExtras();
+
+            if (bundle.getString("NAME") != null) {
+                textView.setText("Olá, " + bundle.getString("NAME")+" seja bem vindo!");
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/comunicacaolayouts/MainActivity.java
+++ app/src/main/java/com/example/comunicacaolayouts/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.comunicacaolayouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,22 +22,52 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        nomeText = findViewById(R.id.nomeText);
-        sobrenomeText = findViewById(R.id.sobrenomeText);
-        idadeText = findViewById(R.id.idadeText);
-        btnEnviar = findViewById(R.id.btnEnviar);
 
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                initViews();
+                validarAntesDeIrHome();
+
 
             }
         });
+    }
 
+    public void validarAntesDeIrHome() {
         String nome = nomeText.getText().toString();
         String sobrenome = nomeText.getText().toString();
-        int idade = Integer.parseInt(nomeText.getText().toString());
+        int idade = Integer.parseInt(idadeText.getText().toString());
+
+        if (nome.isEmpty()) {
+            nomeText.setError("O nome não pode ser vazio!");
+
+
+        }
+        if (sobrenome.isEmpty()) {
+            nomeText.setError("Insira seu sobrenome!");
+
+
+        }
+        if (idade > 0) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            Bundle pacote = new Bundle();
+            pacote.putString("NAME", "Martins");
+            intent.putExtras(pacote);
+            startActivity(intent);
+        } else {
+            System.out.println("Email ou senha invalidos");
+        }
 
 
     }
+
+
+    private void initViews() {
+        nomeText = findViewById(R.id.nomeText);
+        sobrenomeText = findViewById(R.id.sobrenomeText);
+        idadeText = findViewById(R.id.idadeText);
+        btnEnviar = findViewById(R.id.btnEnviar);
+    }
+
 }
